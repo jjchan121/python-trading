cf7e613b3903ad7c0d9bad650400664b28bf7051
Jason Chan <cykjason1618@gmail.com>
2017-07-09 15:47:54 +0800

    fix the context due to api changed

--- scripts/ib_inst_utils.py
+++ scripts/ib_inst_utils.py
@@ -3,31 +3,39 @@ from gevent.event import AsyncResult
 
 monkey.patch_all()
 
-from algotrader.config.app import ApplicationConfig
-from algotrader.config.broker import IBConfig
-from algotrader.config.persistence import MongoDBConfig
-from algotrader.config.persistence import PersistenceConfig
+from algotrader.trading.config import Config, load_from_yaml
+from algotrader.trading.context import ApplicationContext
+from algotrader.utils.logging import logger
+# from algotrader.config.app import ApplicationConfig
+# from algotrader.config.broker import IBConfig
+# from algotrader.config.persistence import MongoDBConfig
+# from algotrader.config.persistence import PersistenceConfig
 from algotrader.provider.broker import Broker
 from algotrader.provider.datastore import PersistenceMode
 from algotrader.provider.datastore import DataStore
 from algotrader.trading.context import ApplicationContext
 from algotrader.trading.ref_data import RefDataManager
 from algotrader.trading.clock import Clock
-from algotrader.utils import logger
+# from algotrader.utils import logger
 
 
-def app_context():
-    persistence_config = PersistenceConfig(None,
-                                           DataStore.Mongo, PersistenceMode.RealTime,
-                                           DataStore.Mongo, PersistenceMode.RealTime,
-                                           DataStore.Mongo, PersistenceMode.RealTime,
-                                           DataStore.Mongo, PersistenceMode.RealTime)
-    app_config = ApplicationConfig(id=None, ref_data_mgr_type=RefDataManager.DB, clock_type=Clock.RealTime,
-                                   persistence_config=persistence_config,
-                                   provider_configs=[MongoDBConfig(), IBConfig(client_id=2, use_gevent=True)])
-    app_context = ApplicationContext(app_config=app_config)
+# def app_context():
+#     persistence_config = PersistenceConfig(None,
+#                                            DataStore.Mongo, PersistenceMode.RealTime,
+#                                            DataStore.Mongo, PersistenceMode.RealTime,
+#                                            DataStore.Mongo, PersistenceMode.RealTime,
+#                                            DataStore.Mongo, PersistenceMode.RealTime)
+#     app_config = ApplicationConfig(id=None, ref_data_mgr_type=RefDataManager.DB, clock_type=Clock.RealTime,
+#                                    persistence_config=persistence_config,
+#                                    provider_configs=[MongoDBConfig(), IBConfig(client_id=2, use_gevent=True)])
+#     app_context = ApplicationContext(app_config=app_config)
+#
+#     return app_context
 
-    return app_context
+config = Config(
+    load_from_yaml("../../config/data_import.yaml"))
+
+app_context = ApplicationContext(config=config)
 
 def init_ib(app_context):
 
